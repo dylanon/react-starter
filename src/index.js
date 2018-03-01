1115146c954b3129dfed9ea42de835fef2400d2d
Dylan On <dylanon@users.noreply.github.com>
2018-02-28 23:09:58 -0500

    Add loaders for css, scss, styles. Enable hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
 class App extends Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 let mode = 'development';
 if (process.env.NODE_ENV === 'production') {
@@ -11,6 +12,7 @@ module.exports = {
         contentBase: './public',
         compress: true,
         host: '0.0.0.0',
+        hot: true,
         port: 3000,
         quiet: true,
         useLocalIp: true
@@ -22,11 +24,39 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
             }
         ]
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
